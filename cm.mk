3aa84691ac12c97ceb693b60ec2a9a771a2564ec
R0rt1z2 <rogerortizleal@gmail.com>
2020-05-13 23:48:15 +0200

    douglas: Don't override the fingerprint.
    * Doesn't makes any sense actually..
    
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- cm.mk
+++ cm.mk
@@ -10,8 +10,3 @@ TARGET_SCREEN_HEIGHT := 800
 PRODUCT_CHARACTERISTICS := tablet
 PRODUCT_NAME := cm_douglas
 PRODUCT_RELEASE_NAME := douglas
-
-# Override the fingerprint for SafetyNet
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=google/mantaray/manta:5.1.1/LMY49J/2640980:user/release-keys \
-    PRIVATE_BUILD_DESC="mantaray-user 5.1.1 LMY49J 2640980 release-keys"
