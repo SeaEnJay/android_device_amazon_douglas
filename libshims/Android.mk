09b7460bbcb3e6fd4e75896b61b9e55199b82e6c
R0rt1z2 <rogerortizleal@gmail.com>
2020-06-14 15:45:38 +0200

    douglas: Organize the libshims folder.
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -1,10 +1,32 @@
 LOCAL_PATH:= $(call my-dir)
 
+# Log
 include $(CLEAR_VARS)
+
 LOCAL_SRC_FILES := log/lab126_shim.cpp
 LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_MODULE := liblog_shim
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+
 include $(BUILD_SHARED_LIBRARY)
 
+LOCAL_PATH:= $(call my-dir)
+
+# Audio
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := audio/libaudio_shim.c
+LOCAL_LDLIBS += -ldl
+
+LOCAL_C_INCLUDES += \
+	external/tinyalsa/include \
+	system/core/include/cutils
+
+LOCAL_SHARED_LIBRARIES := liblog libtinyalsa
+
+LOCAL_MODULE := libaudio_shim
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+
+include $(BUILD_SHARED_LIBRARY)
